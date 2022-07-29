90f37e2b2f2adfa7ed24b2052b81709e853ec943
Victoria <baby_94_777@mail.ru>
2022-07-29 20:58:00 +0300

    написание кода программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+//лучше обойтись исключительно массивами.
+
+char[] delimiterChars = { ',', '.', ':', '"', ' ' };
+string text = "hello, 2, world, ^-)";
+string[] words = text.Split(delimiterChars);
+
+for (int i = 0; i < words.Length; i++)
+{
+string result = string.Empty;
+if (words[i].Length <= 3) 
+{
+    result = result + words[i];
+    Console.Write(result + " ");
+}
+}
